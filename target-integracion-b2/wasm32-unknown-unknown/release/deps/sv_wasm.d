0cda7f1fc1c2b84920bedb92baf983bed550bf68
Juan Antonio Lloret Egea <111652409+juantoniolloretegea@users.noreply.github.com>
2026-08-29 10:29:02 +0000

    integración: retira artefactos de construcción de la candidata

--- target-integracion-b2/wasm32-unknown-unknown/release/deps/sv_wasm.d
+++
@@ -1,7 +0,0 @@
-/home/runner/work/SV-lenguaje-de-computacion/SV-lenguaje-de-computacion/target-integracion-b2/wasm32-unknown-unknown/release/deps/sv_wasm.d: sv_wasm/src/lib.rs
-
-/home/runner/work/SV-lenguaje-de-computacion/SV-lenguaje-de-computacion/target-integracion-b2/wasm32-unknown-unknown/release/deps/sv_wasm.wasm: sv_wasm/src/lib.rs
-
-/home/runner/work/SV-lenguaje-de-computacion/SV-lenguaje-de-computacion/target-integracion-b2/wasm32-unknown-unknown/release/deps/libsv_wasm.rlib: sv_wasm/src/lib.rs
-
-sv_wasm/src/lib.rs:
